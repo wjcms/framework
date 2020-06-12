52ab151788f4a6783a98fe75e90ab9bc744fa2b5
13149563301 <1059114241@qq.com>
2020-06-12 16:42:26 +0800

    修复insert update不生效bug

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,18 +8,18 @@ class HomeController
 {
     public function index()
     {
-        $article = DB::table('article')->insert([
-            'title'=>'laravel',
-            'desc'=>'这是laravel描述'
-        ]);
+        // $article = DB::table('article')->insert([
+        //     'title'=>'laravel',
+        //     'description'=>'这是laravel描述'
+        // ]);
         // $articles = DB::table('article')->get();
         // dump($articles);
-        // $article = DB::table('article')->where([
-        //         ['id','=',7]
-        //     ])->update([
-        //         'title'=>'laravel7.7777',
-        //         'desc'=>'这是laravel描述'
-        //     ]);
+        $article = DB::table('article')->where([
+                ['id','=',7]
+            ])->update([
+                'title'=>'laravel7.7777',
+                'description'=>'这是laravel描述'
+            ]);
         // dump($article);
         // DB::table('article')->where([
         //     ['id','=',5]
